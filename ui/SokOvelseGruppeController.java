e739147496b45fd2830c082c207803d40d9f0701
Oscar Vik <OscarVik@dhcp-10-22-44-77.wlan.ntnu.no>
2018-03-20 15:34:14 +0100

    update in SokOvelseGruppeController

--- ui/SokOvelseGruppeController.java
+++ ui/SokOvelseGruppeController.java
@@ -1,5 +1,3 @@
-
-
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -7,32 +5,41 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ListView;
+import javafx.event.EventHandler;
+import javafx.scene.input.MouseEvent;
 
 public class SokOvelseGruppeController implements Initializable, ControlledScreen{
 
 	ScreensController myController;
+	public String valgtGruppe;
 
-    @FXML
-    private ListView<?> guppe;
-    @FXML
-    private ListView<?> ovelse;
-    
+	@FXML
+	private ListView<String> gruppe;
+	@FXML
+	private ListView<String> ovelse;
+
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-
+		gruppe.setItems(myController.dbController.displayOvelsesgrupper());
+		gruppe.setOnMouseClicked(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent event) {
+				valgtGruppe = gruppe.getSelectionModel().getSelectedItem();
+				System.out.println(valgtGruppe);
+			}
+		});
 	}
 
-    @FXML
-    void goToHome(ActionEvent event) {
-    		myController.setScreen(ApplicationDemo.HomescreenID);
-    }
+	@FXML
+	void goToHome(ActionEvent event) {
+		myController.setScreen(ApplicationDemo.HomescreenID);
+	}
 
 	@Override
 	public void setScreenParent(ScreensController screenPage) {
 		myController = screenPage;
-		
+
 	}
 
 
 }
-
