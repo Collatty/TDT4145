b7ce83308aaa1e86a0f2c05364c1c63ca3059600
Oscar Vik <OscarVik@dhcp-10-22-44-77.wlan.ntnu.no>
2018-03-20 16:43:10 +0100

    jobbet videre med å implementere øvelseiøkt

--- src/OvelseIOkt.java
+++ src/OvelseIOkt.java
@@ -4,16 +4,14 @@ public class OvelseIOkt {
 
     private int oktID;
     private String OvelseID;
-    private int reps;
     private int set;
     private int vekt;
     private int form;
     private int prestasjon;
 
-    public OvelseIOkt(int oktID, String OvesleID, int reps, int set, int vekt, int form, int prestasjon){
+    public OvelseIOkt(int oktID, String OvesleID, int set, int vekt, int form, int prestasjon){
         this.oktID = oktID;
         this.OvelseID= OvesleID;
-        this.reps = reps;
         this.set=set;
         this.vekt=vekt;
         this.form=form;
@@ -27,10 +25,6 @@ public class OvelseIOkt {
 
     public String getOvelseID() {
         return OvelseID;
-    }
-
-    public int getReps() {
-        return reps;
     }
 
     public int getSet() {
